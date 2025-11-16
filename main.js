542c46a52f24e485d0fe9c52ce61a06566acbb07
Diego F. Pajarito Grajales <diegopajarito@gmail.com>
2025-11-16 11:59:21 +0000

    Changing the map style

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ import * as maplibregl from "https://cdn.skypack.dev/maplibre-gl";
 
 const map = new maplibregl.Map({
     container: 'map', // container id
-    style: 'https://demotiles.maplibre.org/style.json', // style URL
+    style: 'https://tiles.openfreemap.org/styles/liberty', // style URL
     center: [0, 0], // starting position [lng, lat]
-    zoom: 1 // starting zoom
+    zoom: 6 // starting zoom
 });
