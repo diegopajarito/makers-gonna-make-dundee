e6073e73c445719cf55c93e109432d6200c9fa4e
Diego F. Pajarito Grajales <diegopajarito@gmail.com>
2025-11-16 11:54:49 +0000

    updated id

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 import * as maplibregl from "https://cdn.skypack.dev/maplibre-gl";
 
 const map = new maplibregl.Map({
-    container: 'mijnkaart', // container id
+    container: 'map', // container id
     style: 'https://demotiles.maplibre.org/style.json', // style URL
     center: [0, 0], // starting position [lng, lat]
     zoom: 1 // starting zoom
