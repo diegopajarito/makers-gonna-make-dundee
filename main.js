aa8870726d83d9ef4a03091eb2c5f2913abf6f4f
Diego F. Pajarito Grajales <diegopajarito@gmail.com>
2025-11-16 12:10:58 +0000

    fixed coords to the place

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ import * as maplibregl from "https://cdn.skypack.dev/maplibre-gl";
 const map = new maplibregl.Map({
     container: 'map', // container id
     style: 'https://tiles.openfreemap.org/styles/liberty', // style URL
-    center: [4.80841, 56.213070], // starting position [lng, lat]
+    center: [-4.80841, 56.213070], // starting position [lng, lat]
     // The Cobler or Ben Arthur 56.21307° N, 4.80841° W
     zoom: 8 // starting zoom
 });
