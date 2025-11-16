68a72fbe2792c1214f42045364ea588d05d9fa6c
Diego F. Pajarito Grajales <diegopajarito@gmail.com>
2025-11-16 12:01:45 +0000

    layers issue coming back to default style

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ import * as maplibregl from "https://cdn.skypack.dev/maplibre-gl";
 
 const map = new maplibregl.Map({
     container: 'map', // container id
-    style: 'https://tiles.openfreemap.org/styles/liberty', // style URL
+    style: 'https://demotiles.maplibre.org/style.json', // style URL
     center: [0, 0], // starting position [lng, lat]
     zoom: 0 // starting zoom
 });
